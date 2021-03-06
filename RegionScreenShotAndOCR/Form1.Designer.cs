e9edee8967da602ce5957e66a6b46718a7c316b5
hieumt <maitrunghieu1995@hotmail.com>
2021-03-07 05:40:27 +0700

    hieumt commit

---
+++ RegionScreenShotAndOCR/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace RegionScreenShotAndOCR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_selectregion = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSetTimeCapture = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_selectregion
+            // 
+            this.btn_selectregion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_selectregion.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.btn_selectregion.Location = new System.Drawing.Point(5, 109);
+            this.btn_selectregion.Name = "btn_selectregion";
+            this.btn_selectregion.Size = new System.Drawing.Size(582, 49);
+            this.btn_selectregion.TabIndex = 1;
+            this.btn_selectregion.Text = "Chọn vùng dữ liệu";
+            this.btn_selectregion.UseVisualStyleBackColor = true;
+            this.btn_selectregion.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(154, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(320, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "SCREEN CAPTURE REGION ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(5, 164);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(582, 277);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Set Time Capture";
+            // 
+            // txtSetTimeCapture
+            // 
+            this.txtSetTimeCapture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSetTimeCapture.Location = new System.Drawing.Point(113, 66);
+            this.txtSetTimeCapture.Name = "txtSetTimeCapture";
+            this.txtSetTimeCapture.Size = new System.Drawing.Size(172, 20);
+            this.txtSetTimeCapture.TabIndex = 7;
+            this.txtSetTimeCapture.Text = "2";
+            this.txtSetTimeCapture.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSetTimeCapture_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(291, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "( min )";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.ForeColor = System.Drawing.Color.Crimson;
+            this.btnStop.Location = new System.Drawing.Point(5, 447);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(582, 49);
+            this.btnStop.TabIndex = 9;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 504);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSetTimeCapture);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_selectregion);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Screen Capture Region";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_selectregion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSetTimeCapture;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ RegionScreenShotAndOCR/Form1.cs
@@ -0,0 +1,177 @@
+﻿using GI.Screenshot;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+
+namespace RegionScreenShotAndOCR
+{
+    public partial class Form1 : Form
+    {
+        //set variable global
+        Bitmap preBitMap = null;
+        int width = 0;
+        int height = 0;
+        int stride = 0;
+        int x = 0;
+        int y = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btn_selectregion.Enabled = false;
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+            if (!String.IsNullOrEmpty(txtSetTimeCapture.Text))
+            {
+                double timeLoop = Convert.ToDouble(txtSetTimeCapture.Text);
+                _ = RunPeriodically(TimeSpan.FromMinutes(timeLoop), tokenSource.Token);
+            }
+            else
+            {
+                MessageBox.Show("Nhập thời gian định kỳ ! ");
+                txtSetTimeCapture.Focus();
+            }
+        }
+
+        /// <summary>
+        /// Convert Image to bitmap
+        /// </summary>
+        /// <param name="bitmapSource"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <param name="stride"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static Bitmap ConvertToBitmap(BitmapSource bitmapSource, int width, int height, int stride,int x, int y)
+        {
+            var memoryBlockPointer = Marshal.AllocHGlobal(height * stride);
+            bitmapSource.CopyPixels(new Int32Rect(x, y, width, height), memoryBlockPointer, height * stride, stride);
+            var bitmap = new Bitmap(width, height, stride, PixelFormat.Format32bppArgb, memoryBlockPointer);
+            return bitmap;
+        }
+
+
+        /// <summary>
+        /// RunPeriodically
+        /// </summary>
+        /// <param name="interval"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        async Task RunPeriodically( TimeSpan interval, CancellationToken token)
+        {
+            while (true)
+            {
+                this.WindowState = FormWindowState.Minimized;
+                Thread.Sleep(500);
+                captureScreenAndCompare();
+                await Task.Delay(interval, token);
+            }
+        }
+
+        /// <summary>
+        /// Capture screen region and notice of difference
+        /// </summary>
+        public void captureScreenAndCompare()
+        {
+           
+            if (width == 0 && height == 0 && stride == 0)
+            {
+                BitmapSource bitmapSourceRegion = Screenshot.CaptureRegion();
+                width = bitmapSourceRegion.PixelWidth;
+                height = bitmapSourceRegion.PixelHeight;
+                x = ((CroppedBitmap)bitmapSourceRegion).SourceRect.X;
+                y = ((CroppedBitmap)bitmapSourceRegion).SourceRect.Y;
+                stride = width * ((bitmapSourceRegion.Format.BitsPerPixel + 7) / 8);
+            }
+            BitmapSource bitmapSource = Screenshot.CaptureAllScreens();
+            Bitmap currentBitMap = ConvertToBitmap(bitmapSource, width, height, stride, x, y);
+
+            pictureBox1.Image = (Image)currentBitMap;
+            this.WindowState = FormWindowState.Normal;
+            if (preBitMap != null)
+            {
+               Boolean isTheSame = CompareBitmapsFast(preBitMap, currentBitMap);
+                if (!isTheSame)
+                {
+                    preBitMap = currentBitMap;
+                    MessageBox.Show("Có sự khác biệt");
+                }
+            }
+            else
+            {
+                preBitMap = currentBitMap;
+            }
+
+        }
+        /// <summary>
+        /// compare bitmap
+        /// </summary>
+        /// <param name="bmp1"></param>
+        /// <param name="bmp2"></param>
+        /// <returns></returns>
+        public static bool CompareBitmapsFast(Bitmap bmp1, Bitmap bmp2)
+        {
+            if (bmp1 == null || bmp2 == null)
+                return false;
+            if (object.Equals(bmp1, bmp2))
+                return true;
+            if (!bmp1.Size.Equals(bmp2.Size) || !bmp1.PixelFormat.Equals(bmp2.PixelFormat))
+                return false;
+
+            int bytes = bmp1.Width * bmp1.Height * (Image.GetPixelFormatSize(bmp1.PixelFormat) / 8);
+
+            bool result = true;
+            byte[] b1bytes = new byte[bytes];
+            byte[] b2bytes = new byte[bytes];
+
+            BitmapData bitmapData1 = bmp1.LockBits(new Rectangle(0, 0, bmp1.Width, bmp1.Height), ImageLockMode.ReadOnly, bmp1.PixelFormat);
+            BitmapData bitmapData2 = bmp2.LockBits(new Rectangle(0, 0, bmp2.Width, bmp2.Height), ImageLockMode.ReadOnly, bmp2.PixelFormat);
+
+            Marshal.Copy(bitmapData1.Scan0, b1bytes, 0, bytes);
+            Marshal.Copy(bitmapData2.Scan0, b2bytes, 0, bytes);
+
+            for (int n = 0; n <= bytes - 1; n++)
+            {
+                if (b1bytes[n] != b2bytes[n])
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            bmp1.UnlockBits(bitmapData1);
+            bmp2.UnlockBits(bitmapData2);
+
+            return result;
+        }
+        /// <summary>
+        /// event key press: not press character
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtSetTimeCapture_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
